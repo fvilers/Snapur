cd7469e98e7fcf6b5a3295f78b5ec92405bae027
Fabian Vilers <fabian.vilers@dev-one.com>
2014-09-17 21:56:04 +0200

    Added upload to Imgur call

--- src/snapur.js
+++ src/snapur.js
@@ -12,7 +12,31 @@ $(function () {
             };
         
         chrome.tabs.captureVisibleTab(currentTab.windowId, options, function (dataUrl) {
-            chrome.tabs.create({ url: dataUrl }, function () {
+            $message.text('Uploading snapshot to Imgur...');
+            
+            $.ajax({
+                url: 'https://api.imgur.com/3/image',
+                type: 'POST',
+                headers: {
+                    Authorization: 'Client-ID f957f05bea2a82a'
+                },
+                data: {
+                    image: dataUrl.split(',')[0],
+                    type: 'base64',
+                    title: currentTab.title,
+                    description: 'Snaped from ' + currentTab.url
+                },
+                dataType: 'json'
+            }).done(function (json) {
+                $message.text('Opening snapshot from Imgur...');
+                    
+                chrome.tabs.create({ url: json.data.link }, function () {
+                    $message.text('');
+                });
+            }).fail(function (xhr) {
+                var response = JSON.parse(xhr.responseText);
+                
+                $('#message').text(response.data.error).addClass('error');
             });
         });
     });
