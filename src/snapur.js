fc30647a4cc695a322a839828e82a9e01279d3d8
Fabian Vilers <fabian.vilers@dev-one.com>
2014-09-18 10:25:04 +0200

    Removed tabs permissions

--- src/snapur.js
+++ src/snapur.js
@@ -3,41 +3,37 @@
 (function () {
     'use strict';
     
-    var message = document.getElementById('message');
-    
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        var currentTab = tabs[0],
-            options = {
-                format: 'png'
-            };
-        
-        chrome.tabs.captureVisibleTab(currentTab.windowId, options, function (dataUrl) {
-            message.innerHTML = 'Uploading snapshot to Imgur...';
-            
-            var httpRequest = new XMLHttpRequest(),
-                data = { image: dataUrl.split(",")[1]  };
+    var message = document.getElementById('message'),
+        options = {
+            format: 'png'
+        };
 
-            httpRequest.onreadystatechange = function () {
-                if (httpRequest.readyState === 4) {
-                    var response = JSON.parse(httpRequest.responseText);
-                    
-                    if (httpRequest.status === 200) {
-                        message.innerHTML = 'Opening snapshot from Imgur...';
-                
-                        chrome.tabs.create({ url: response.data.link }, function () {
-                            message.innerHTML = '';
-                        });
-                    } else {
-                        message.innerHTML = response.data.error;
-                        message.classList.add('error');
-                    }
+    chrome.tabs.captureVisibleTab(null, options, function (dataUrl) {
+        message.innerHTML = 'Uploading snapshot to Imgur...';
+
+        var httpRequest = new XMLHttpRequest(),
+            data = { image: dataUrl.split(",")[1]  };
+
+        httpRequest.onreadystatechange = function () {
+            if (httpRequest.readyState === 4) {
+                var response = JSON.parse(httpRequest.responseText);
+
+                if (httpRequest.status === 200) {
+                    message.innerHTML = 'Opening snapshot from Imgur...';
+
+                    chrome.tabs.create({ url: response.data.link }, function () {
+                        message.innerHTML = '';
+                    });
+                } else {
+                    message.innerHTML = response.data.error;
+                    message.classList.add('error');
                 }
-            };
+            }
+        };
 
-            httpRequest.open("POST", "https://api.imgur.com/3/upload");
-            httpRequest.setRequestHeader("Authorization", "Client-ID 580e21bb1463b5b");
-            httpRequest.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-            httpRequest.send(JSON.stringify(data));
-        });
+        httpRequest.open("POST", "https://api.imgur.com/3/upload");
+        httpRequest.setRequestHeader("Authorization", "Client-ID 580e21bb1463b5b");
+        httpRequest.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        httpRequest.send(JSON.stringify(data));
     });
 }());
